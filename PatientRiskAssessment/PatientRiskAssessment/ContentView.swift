63aa50562d0b8c7d6dab8f0ef08b74257951c98f
Rastaar Haghi <rhaghi@usc.edu>
2020-04-25 22:40:39 -0700

    initial commit

---
+++ PatientRiskAssessment/PatientRiskAssessment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PatientRiskAssessment
+//
+//  Created by Rastaar Haghi on 4/25/20.
+//  Copyright © 2020 Rastaar Haghi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
